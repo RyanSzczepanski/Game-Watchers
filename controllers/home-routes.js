a26f0e8b21a72d2f247d65a266005f1a8df5deae
Ryan Szczepanski <rszczepanski@comcast.net>
2021-09-01 17:56:34 -0400

    Final touches

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,26 +1,21 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Post, User, Comment, Vote, GameList, Game } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');
-  Post.findAll({
+  GameList.findAll({
     attributes: [
       'id',
-      'post_url',
       'title',
+      'user_id',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
+        model: Game,
+        attributes: ['id', 'game_title', 'created_at'],
       },
       {
         model: User,
@@ -28,11 +23,10 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-
-      res.render('homepage', {
-        posts,
+    .then(dbGamelistData => {
+      const gamelists = dbGamelistData.map(gamelist => gamelist.get({ plain: true }));
+      res.render('homepage',{
+        gamelists,
         loggedIn: req.session.loggedIn
       });
     })
